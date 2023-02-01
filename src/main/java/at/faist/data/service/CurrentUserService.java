69f28a4fc51b8a01cd7cfddcf3cc8bad9538fe8d
ffaist <florian.faist@faktorzehn.at>
2023-02-01 17:43:19 +0100

    read security context

--- src/main/java/at/faist/data/service/CurrentUserService.java
+++ src/main/java/at/faist/data/service/CurrentUserService.java
@@ -1,11 +1,14 @@
 package at.faist.data.service;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CurrentUserService {
 
     public String getCurrentUser() {
-        return "demo";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
     }
 }
