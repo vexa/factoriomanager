2d31ef66f8c87cc8234b64901069dc796b11bcac
ffaist <florian.faist@faktorzehn.at>
2023-02-02 08:16:58 +0100

    fix test mapping subnet json

--- src/main/java/at/faist/data/model/docker/Config.java
+++ src/main/java/at/faist/data/model/docker/Config.java
@@ -1,4 +1,13 @@
 package at.faist.data.model.docker;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = SubnetConfig.class, name = "subnet")})
 public interface Config {
 }
